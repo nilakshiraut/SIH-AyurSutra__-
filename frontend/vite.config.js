f8a17bac6d69ed4d13d35663fba34aaa428ee4d7
nilakshiraut <nilakshiraut0@gmail.com>
2025-12-05 20:05:01 +0530

    Updated frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://sih-ayursutra.onrender.com',
+       
         changeOrigin: true
       },
       '/ws': {
-        target: 'wss://sih-ayursutra.onrender.com',
+      
         ws: true
       }
     }
